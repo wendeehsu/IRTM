82bb690e3854cd87a99112f8e42b94324eb1b087
Melhor <b05705046@ntu.edu.tw>
2019-12-29 15:58:38 +0800

    get ingredients

--- project/recipeCrawler.py
+++ project/recipeCrawler.py
@@ -1,24 +1,28 @@
 import scrapy
 
+def Cleaning(rawList):
+	if rawList == "" or rawList == " ":
+		return False
+	return True
+
 class RecipeSpider(scrapy.Spider):
     name = "recipe_spider"
     start_urls = ['https://www.yummly.com/recipes']
 
     def parse(self, response):
         SET_SELECTOR = ".recipe-card"
-        NAME_SELECTOR = ".card-title::text"
         URL_SELECTOR = ".card-title::attr(href)"
-        print("response.url = " + response.url)
 
         for recipe in response.css(SET_SELECTOR):
-        	name = recipe.css(NAME_SELECTOR).extract_first()
         	link = "https://www.yummly.com" + recipe.css(URL_SELECTOR).extract_first()
-        	print(link)
-        	# request = response.follow(link, callback=self.parse)
-         #    # Return it thanks to a generator
-         #    yield request
-            # yield {
-            #     'url': recipe.css(URL_SELECTOR).extract_first(),
-            #     'name': recipe.css(NAME_SELECTOR).extract_first()
-            # }
+        	request = response.follow(link, callback=self.parseRecipe)
+        	yield request
+    
+    def parseRecipe(self, response):
+    	recipeName = response.css(".recipe-title::text").extract_first()
+    	ingredients = response.css(".ingredient::text").extract()
+    	ingredients = list(filter(Cleaning,ingredients))
+    	print("recipeName = ", recipeName)
+    	print(ingredients)
+
             
